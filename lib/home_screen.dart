7b84455c9ab13d7a644066a71708a25a8962aead
Your Name <your_email@example.com>
2025-12-28 10:01:25 +0200

    implement nav bar logic

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:islamic_app/nav_bar_selected_icon.dart';
 import 'package:islamic_app/nav_bar_unselected_icon.dart';
+import 'package:islamic_app/tabs/hadeth/hadeth_tab.dart';
+import 'package:islamic_app/tabs/quran/quran_tab.dart';
+import 'package:islamic_app/tabs/radio/radio_tab.dart';
+import 'package:islamic_app/tabs/sebha/sebha_tab.dart';
+import 'package:islamic_app/tabs/time/time_tab.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  
   static const String routeName = '/home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+int currentIndex = 0;
+List<Widget> tabs = [
+  QuranTab(),
+  HadethTab(),
+  SebhaTab(),
+  RadioTab(),
+  TimeTab()
+  
+];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: tabs[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          currentIndex = index;
+          setState(() {
+            
+          });
+        },
         items: [//مينفعش يكون فيها أأقل من اتنين و ده طبيعي
           BottomNavigationBarItem(
             icon: NavBarUnselectedIcon(imageName: 'quran'),
